b5d6f7a392be2a049770a690dc5025d77a710bce
Domenico Morabito <43889666+11dome11@users.noreply.github.com>
2021-03-25 21:33:37 +0100

    Add files via upload

--- Lucy.py
+++ Lucy.py
@@ -8,6 +8,8 @@ import webbrowser
 import subprocess
 import winsound
 import os
+import requests
+import psutil 
 
 listener= sr.Recognizer()
 engine= pyttsx3.init()
@@ -22,7 +24,6 @@ def talk(text):
     engine.runAndWait()
     
 def take_command():
-    try:
         with sr.Microphone() as source:
             print("Sto ascoltando...")
             voice= listener.record(source,duration=3)
@@ -30,10 +31,29 @@ def take_command():
             command= command.lower()
             if 'lucy' in command:
                 command = command.replace('lucy', '')
-                print(command)           
+
+        return command
+    
+def get_weather(city):
+    weather_key= "768f0e227a4f91db43e5ed9d29f1499b"
+    url= "http://api.openweathermap.org/data/2.5/weather"
+    params= {"appid":weather_key, "q":city, "units":"metric", "lang":"it"}
+    response = requests.get(url,params=params)
+    weather= response.json()
+    format_response(weather)
+
+def format_response(weather):
+    try:
+        condition = weather["weather"][0]["description"]
+        temperature = weather["main"]["temp"] 
+        temperature = int(temperature)
+        humidity = weather ["main"]["humidity"]
+        wind = weather["wind"]["speed"]
+        wind= int(wind)
+        final_str= "Condizioni: {}, Temperatura: {} gradi, Umidità: {}%, Vento: {} chilometri orari". format(condition,temperature,humidity,wind)
     except:
-        pass
-    return command
+        final_str= "Non sono riuscita a trovare le informazioni"
+    talk(final_str)
 
 def note(text):
     date= datetime.datetime.now()
@@ -52,8 +72,6 @@ def sveglia(text):
         if alarm==now:
             winsound.PlaySound("alarmclock.wav", winsound.SND_ASYNC)
             break
-        else:
-            pass
         
 def find_all(name, path):
     result = []
@@ -70,17 +88,35 @@ def run_lucy():
         talk("Quale canzone?")
         song=take_command()
         talk(song)
+        print(song)
         pywhatkit.playonyt(song)
+    elif "meteo" in command:
+        talk("Quale città?")
+        città=take_command()
+        print(città)
+        get_weather(città)
+    elif "spegni" in command:
+        os.system("shutdown /s /t 1")
+    elif "riavvia" in command:
+        os.system("shutdown /r /t 1")
+    elif "batteria" in command:
+        battery= psutil.sensors_battery()
+        percent= battery.percent
+        percent= str(percent)
+        per= percent + "per cento"
+        talk(per)
     elif "chi è" in command:
         wikipedia.set_lang("it")
         person= command.replace("chi è","")
         info= wikipedia.summary(person,2)
         talk(info)
+        print(info)
     elif "che cos'è" in command:
         wikipedia.set_lang("it")
         thing= command.replace("che cos'è","")
         info2= wikipedia.summary(thing,2)
         talk(info2)
+        print(info2)
     elif "jokes" in command:
         talk(pyjokes.get_joke())
     elif "ore" in command:
@@ -90,14 +126,17 @@ def run_lucy():
     elif "giorno" in command:
         giorno= datetime.date.today().strftime("%d/%m/%Y")
         talk(giorno)
+        print(giorno)
     elif "cerca su google" in command:
         talk('Cosa devo cercare?')
         search=take_command()
+        print(search)
         url= "https://www.google.com/search?q=" + search
         webbrowser.get().open(url)
     elif "cerca un luogo" in command:
         talk('Quale luogo?')
         luogo=take_command()
+        print(luogo)
         url= "https://www.google.nl/maps/place/" + luogo +"/&amp"
         webbrowser.get().open(url)
     elif "scrivi una nota" in command:
@@ -113,6 +152,9 @@ def run_lucy():
         orario_x= datetime.datetime.strptime(orario, " %H:%M").time()
         print(orario_x)
         sveglia(orario_x)
+    elif "ciao " or "buonasera" or "buondì" in command:
+        talk("Ciao, dimmi")
+        run_lucy()
     else:
         talk('Perfavore ripeti il comando')
         run_lucy()
