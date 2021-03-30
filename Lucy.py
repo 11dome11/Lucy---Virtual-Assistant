0ba185c056a658f2c86ecbd862f44b461305843e
Domenico Morabito <43889666+11dome11@users.noreply.github.com>
2021-03-30 22:13:13 +0200

    Add files via upload

--- Lucy.py
+++ Lucy.py
@@ -10,6 +10,7 @@ import winsound
 import os
 import requests
 import psutil 
+import pyautogui as pg
 
 listener= sr.Recognizer()
 engine= pyttsx3.init()
@@ -23,26 +24,29 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
     
-def take_command():
+def take_command(wa=False,numero=False):
         with sr.Microphone() as source:
             print("Sto ascoltando...")
-            voice= listener.record(source,duration=3)
-            command= listener.recognize_google(voice, language="it-IT")
-            command= command.lower()
+            if wa:
+                voice= listener.record(source,duration=6)
+                command= listener.recognize_google(voice, language="it-IT")
+                command= command.lower()
+                if numero:
+                    command = command.replace(" ","")
+            else:
+                voice= listener.record(source,duration=4)
+                command= listener.recognize_google(voice, language="it-IT")
+                command= command.lower()
             if 'lucy' in command:
                 command = command.replace('lucy', '')
-
         return command
-    
+
 def get_weather(city):
     weather_key= "768f0e227a4f91db43e5ed9d29f1499b"
     url= "http://api.openweathermap.org/data/2.5/weather"
     params= {"appid":weather_key, "q":city, "units":"metric", "lang":"it"}
     response = requests.get(url,params=params)
     weather= response.json()
-    format_response(weather)
-
-def format_response(weather):
     try:
         condition = weather["weather"][0]["description"]
         temperature = weather["main"]["temp"] 
@@ -53,7 +57,7 @@ def format_response(weather):
         final_str= "Condizioni: {}, Temperatura: {} gradi, Umidità: {}%, Vento: {} chilometri orari". format(condition,temperature,humidity,wind)
     except:
         final_str= "Non sono riuscita a trovare le informazioni"
-    talk(final_str)
+    return final_str
 
 def note(text):
     date= datetime.datetime.now()
@@ -72,18 +76,11 @@ def sveglia(text):
         if alarm==now:
             winsound.PlaySound("alarmclock.wav", winsound.SND_ASYNC)
             break
-        
-def find_all(name, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            result.append(os.path.join(root, name))
-    return result
             
 #MAIN
 def run_lucy():
     command= take_command()
-    print(command)
+    print("Hai detto:" +command)
     if "una canzone" in command:
         talk("Quale canzone?")
         song=take_command()
@@ -94,7 +91,8 @@ def run_lucy():
         talk("Quale città?")
         città=take_command()
         print(città)
-        get_weather(città)
+        previsioni=get_weather(città)
+        talk(previsioni)
     elif "spegni" in command:
         os.system("shutdown /s /t 1")
     elif "riavvia" in command:
@@ -105,18 +103,27 @@ def run_lucy():
         percent= str(percent)
         per= percent + "per cento"
         talk(per)
+    elif "messaggio" in command:
+        now=datetime.datetime.now().time()
+        talk("Cosa vuoi scrivere?")
+        mess=take_command(wa=True)
+        talk("A che numero?")
+        num=take_command(wa=True,numero=True)
+        print("Numero del destinatario:" + num)
+        pywhatkit.sendwhatmsg("+39" + num, mess, int(now.hour), int(now.minute)+2)
+        width,height = pg.size()
+        pg.click(width/2,height/2)
+        pg.press("enter")
     elif "chi è" in command:
         wikipedia.set_lang("it")
         person= command.replace("chi è","")
         info= wikipedia.summary(person,2)
         talk(info)
-        print(info)
     elif "che cos'è" in command:
         wikipedia.set_lang("it")
         thing= command.replace("che cos'è","")
         info2= wikipedia.summary(thing,2)
         talk(info2)
-        print(info2)
     elif "jokes" in command:
         talk(pyjokes.get_joke())
     elif "ore" in command:
